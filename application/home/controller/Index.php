526a0f3de0df1e13e33a594a718e34b94521524c
stiff <15700004138@163.com>
2017-09-27 10:42:36 +0800

    增加首页列表展示

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -7,12 +7,9 @@
  */
 
 namespace app\home\controller;
-use app\admin\model\Picture;
-use think\Controller;
-use app\home\model\News as NewsModel;
-use app\home\model\Learn as LearnModel;
+use app\home\model\Picture;
 use app\home\model\Notice as NoticeModel;
-use app\home\model\Pioneer as PioneerModel;
+use think\Db;
 
 
 /**
@@ -23,13 +20,39 @@ class Index extends Base {
      * 首页
      * @return mixed
      */
-    public function index(){
+    public function index()
+    {
         $this ->anonymous();
         $uid = session('userId');
         $this ->assign('user',$uid);
-
         $this->assign('test',0);
+
         return $this->fetch();
     }
 
+    public function moreDataList()
+    {
+        $len = input('length');
+        $res = Db::field('title,create_time,publisher,id,front_cover,class_type,type')
+            ->table('pb_notice')
+            ->where('type!=1 and status>=0') // notice type1 没有发布人
+            ->union("SELECT title,create_time,publisher,id,front_cover,class_type,type FROM pb_learn")
+            ->union("SELECT title,create_time,publisher,id,front_cover,class_type,collect as type FROM pb_news")
+            ->union("SELECT title,create_time,publisher,id,front_cover,class_type,type FROM pb_centraltask where status>=0 order by create_time desc limit $len,7")
+            ->select();
+
+        foreach ($res as $k=>$v) {
+            $img = Picture::get($v['front_cover']);
+            $res[$k]['path'] = $img['path'];
+            $res[$k]['time'] = date("Y-m-d",$v['create_time']);
+        }
+
+        if (empty($res)) {
+
+            return $this->error('数据为空!');
+        } else {
+
+            return $this->success('数据获取成功!',null,$res);
+        }
+    }
 }
