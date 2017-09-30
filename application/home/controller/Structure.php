ee954a089c25b8ebdf22165c0119c27e5d02e553
stiff <15700004138@163.com>
2017-09-30 10:38:38 +0800

    bug修改

--- application/home/controller/Structure.php
+++ application/home/controller/Structure.php
@@ -14,6 +14,8 @@ class Structure extends Base{
      * 组织架构主页
      */
     public function index(){
+        $this->assign('title',config('title'));
+
         return $this->fetch();
     }
     /*
